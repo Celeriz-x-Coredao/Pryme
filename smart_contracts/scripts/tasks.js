6582df78f2f2df053147078029a12f1cc59c5cd6
Debasish Behera <debasishbehera280@gmail.com>
2025-02-04 20:15:07 -0800

    tasks

---
+++ smart_contracts/scripts/tasks.js
@@ -0,0 +1,32 @@
+const { task } = require("hardhat/config");
+const config = require("./config");
+
+task("deploy-verifiers", "Deploys all verifier contracts")
+  .setAction(async (taskArgs, hre) => {
+    const deploy = require("./scripts/deploy.js").deploy;
+    await deploy();
+  });
+
+task("verify-deployment", "Verifies all deployed contracts")
+  .setAction(async (taskArgs, hre) => {
+    await require("./scripts/verify.js");
+  });
+
+task("test-verifiers", "Tests all verifier contracts")
+  .setAction(async (taskArgs, hre) => {
+    await hre.run("test");
+  });
+
+task("show-gas", "Shows gas usage for deployments")
+  .setAction(async (taskArgs, hre) => {
+    const utils = require("./utils");
+    const contracts = [
+      "DebtToIncomeVerifier",
+      "TransactionHistoryVerifier",
+      "WalletAgeVerifier",
+      "WalletBalanceVerifier",
+      "CreditScoring"
+    ];
+
+   
+  });
